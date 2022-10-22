d801ac180b12b7c60f16c319b8b913608f077816
HamdaanAliQuatil <96776914+HamdaanAliQuatil@users.noreply.github.com>
2022-10-22 21:48:54 +0530

    feat: create components for speaker, speaker image and speaker demographics

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,13 @@
 import { data } from "../SpeakerData";
-
-function Session(props){ 
-  //DRY - Don't Repeat Yourself - {title, room} can be passed as params as well
-  const {title, room} = props; 
-  return(
-    <span className="session w-100">
-    {title} <strong> Room: {room}</strong>
-  </span>
-  );
-}
-
-function Sessions({sessions}) {
-  return (
-    <div className="sessionBox card h-250">
-    <Session title={sessions[0].title} room={sessions[0].room.name} />
-  </div>
-  );
-}
-
-// refactored component Sessions which includes other refactored component Session
-// IndexPage is the parent component. It consumes Sessions component and Sessions consumes Session component
-
+import Speaker from "../src/components/Speaker";
 
 const IndexPage = () => {
   return (
-    <div className="conatiner spaekers-list">
+    <div className="container speakers-list">
       <div className="row">
-          {data.map(function (speaker) {
-            const { id, bio, first, last, favorite, twitterHandle, company, sessions, } = speaker;
-            return (
-              <div 
-              key = {id}
-              className="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-sm-12 col-xs-12">
-              <div className="card card-height p-4 mt-4">
-                <div className="speaker-img d-flex flex-row justify-content-center align-items-center h-300">
-                    <img
-                      className="conatin-fit"
-                      src={`/images/speaker-${id}.jpg`}
-                      width="300"
-                      alt={`${first} ${last}`}
-                    />
-                    </div>
-
-                    <div className="speaker-info">
-                      <div className="d-flex justify-content-between mb-3">
-                        <h3 className="text-truncate w-200">
-                          {first} {last}
-                        </h3>
-                      </div>
-                      <div className="speaker-company text-truncate w-200">
-                        <p>{bio} {company} {twitterHandle} {favorite}</p>
-                      </div>
-                    </div>
-
-                  {/* component to display conference details */}
-                  <Sessions sessions={sessions} />
-
-              </div>
-            </div>
-            );
-          })}
+        {data.map(function (speaker) {
+          return <Speaker key={speaker.id} speaker={speaker} />;
+        })}
       </div>
     </div>
   );
