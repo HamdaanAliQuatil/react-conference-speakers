b973225a75f2451e5d6d2eb3f9d40abb2e98bd19
HamdaanAliQuatil <96776914+HamdaanAliQuatil@users.noreply.github.com>
2022-10-24 16:05:45 +0530

    feat: add header component

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8bd95b25777586b4"; }
+/******/ 		__webpack_require__.h = function() { return "5681682d31f6aa62"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
